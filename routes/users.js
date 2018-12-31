9b123fc4af44c03ec89f74b3cdce937f31d786c6
romarios1987 <romios047@gmail.com>
2018-12-31 22:33:01 +0200

    adding user to database

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
+
+// User Model
+const User = require('../models/User');
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -43,10 +47,52 @@ router.post('/register', (req, res) => {
             password2
         })
     } else {
-        res.send('Passed')
+        // Validation passed
+        User.findOne({email: email})
+            .then(user => {
+                if (user) {
+                    // User exists
+                    errors.push({msg: 'Email is already registered'});
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    })
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                    // console.log(newUser);
+                    // res.send('Hello')
+
+                    // Hash password
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err;
+
+                            // set password to hashed
+                            newUser.password = hash;
+
+                            // console.log(newUser);
+                            // res.send('Hello')
+
+                            // Save User
+                            newUser.save()
+                                .then(() => {
+                                    res.redirect('/users/login');
+                                })
+                                .catch(err => console.log(err))
+                        })
+                    })
+
+                }
+            })
     }
-
-
 });
 
 module.exports = router;
