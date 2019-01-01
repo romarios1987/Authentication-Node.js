498fbe5e6a00c5159550b5f63d368051e01b0fb7
romarios1987 <romios047@gmail.com>
2019-01-01 19:17:27 +0200

    LogIn & LogOut user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-
+const passport = require('passport');
 
 // User Model
 const User = require('../models/User');
@@ -97,9 +97,21 @@ router.post('/register', (req, res) => {
 });
 
 
+// Login Handle
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
+});
 
-
-// Register Handle
+// Logout Handle
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login')
+});
 
 
 module.exports = router;
